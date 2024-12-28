f86c2ba78bd81ecfa323164fd9bfb9b79fa4c7ed
abhisheksonias <abhisheksonisince2003@gmail.com>
2024-12-28 15:14:59 +0530

    form updated

--- K7international/assets/js/script.js
+++ K7international/assets/js/script.js
@@ -55,28 +55,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var modal = document.getElementById("bookingModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("bookSeatBtn");
+// Get all the "Book Now" buttons
+var buttons = document.querySelectorAll(".btn");
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+// When the user clicks any "Book Now" button, open the modal
+buttons.forEach(function(btn) {
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+});
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
     modal.style.display = "none";
-  }
+}
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+        modal.style.display = "none";
     }
-  }
+}
+
 /**
  * header & go-top-btn active
  * when window scroll down to 400px
